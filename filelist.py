8f2f7763f7f36c3e07934e918199f965ee54b1c9
nicciniamh <nicci@BigMac.local>
2020-06-10 22:14:38 -0700

    Fixed the testing code. Edit for your data

--- filelist.py
+++ filelist.py
@@ -3,12 +3,22 @@ class list(list):
 	class that extends list to initialize from an optional file. If no filename
 	is specified a simple list is initialized. 
 	'''
-	def __init__(self,filename=False):
-		if filename:
-			self.filename = filename
-			with open(filename) as f:
-				self.extend(f.read().splitlines())
+	def __init__(self,arg=False):
+		self.initialize(arg)
+
+	def initialize(self,arg=False):
+		if not arg:
+			filename = '/dev/null'
+		else:
+			filename = arg
+
+		with open(filename) as f:
+			self.extend(f.read().splitlines())
+
 
 if __name__ == '__main__':
-	foo = filelist('data')
+	import os
+	from importlib.machinery import SourceFileLoader
+	mod = SourceFileLoader("filelist", "filelist.py").load_module()
+	foo = mod.list(os.path.expanduser('~/data'))
 	print('foo is {}: {}'.format(type(foo), ', '.join(foo)))
